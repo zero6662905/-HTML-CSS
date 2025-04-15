8c62b1a793fd3ac1e9a6339a1af9fc7f40b3c2b1
zero6662905 <egor.kov2905@gmail.com>
2025-04-15 22:30:24 +0300

    Python web frameworks try 2

--- task.py
+++ task.py
@@ -7,7 +7,7 @@ print(f"{DATABASE} created")
 
 
 query_create = """
-CREATE TABLE restaurant (
+CREATE TABLE restaurants (
     id INT PRIMARY KEY,
     address VARCHAR(100) NOT NULL,
     number_of_Michelin_stars TINYINT UNSIGNED NOT NULL,
@@ -16,7 +16,15 @@ CREATE TABLE restaurant (
     );
 """
 
+query_insert = """
+INSERT INTO restaurants (id, address, number_of_Michelin_stars, rating, culinary_tradition_of_the_restaurant)
+VALUES (1, 'Via Stella, 22, 41121 Modena MO, Італія', 3, '4,4', 'Італійська'),
+(2, '9 Pl. des Vosges, 75004 Paris, Франція', 3, '4,1', 'Французька'),
+(3, 'Carrer de Can Sunyer, 48, 17007 Girona, Іспанія', 3, '4,8', 'Іспанська')
+"""
+
 conn.execute(query_create)
+conn.execute(query_insert)
 conn.commit()
 
 conn.close()
