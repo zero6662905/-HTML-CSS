9b0ad1c5bd720cb12c7e2af26b91a74e0b4b417a
zero6662905 <egor.kov2905@gmail.com>
2025-06-24 12:33:20 +0300

    bootstrap

--- task_1.py
+++ task_1.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/countries')
-def countries():
-    return render_template('countries.html')
+@app.route("/")
+def main():
+    return "<h1>Hello, world!</h1>"
 
-@app.route('/contact')
+@app.route("/about/")
+def about():
+    return "<h1>Про нас</h1>"
+
+@app.route("/services/")
+def services():
+    return "<h1>опис послуг</h1>"
+
+@app.route("/contact/")
 def contact():
-    return render_template('contact.html')
+    return "<h1>Контактна сторінка з інформацією про зв'язок</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
